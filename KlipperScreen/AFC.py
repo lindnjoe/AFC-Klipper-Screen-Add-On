c7ee855f93639c6a4f4b2d611b30f82b1f420945
Eric Sears <esears@gmail.com>
2025-04-22 07:42:51 -0400

    Add installation script and update README for AFC-Klipper-Screen-Add-On

--- KlipperScreen/AFC.py
+++ KlipperScreen/AFC.py
@@ -1,3 +1,9 @@
+# Armored Turtle Automated Filament Control
+#
+# Copyright (C) 2024-2025 Armored Turtle
+#
+# This file may be distributed under the terms of the GNU GPLv3 license.
+
 import logging
 import os.path
 import gi
